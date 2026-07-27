b6c323509df796b7c891ab880655c427b826f495
bawabet-altamreed <medo.beso2023@gmail.com>
2026-07-27 23:53:46 +0300

    Update admin-dashboard.js

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -1,1 +1,26 @@
 alert("أنا داخل الملف الصحيح");
+
+firebase.auth().onAuthStateChanged(function(user){
+
+    if(!user){
+        alert("مفيش مستخدم مسجل دخول");
+        return;
+    }
+
+    alert("المستخدم: " + user.email);
+
+    db.collection("users").get()
+
+    .then(function(snapshot){
+
+        alert("عدد المستخدمين = " + snapshot.size);
+
+    })
+
+    .catch(function(error){
+
+        alert("الخطأ: " + error.message);
+
+    });
+
+});
